90e278bb02e259cdd37ce27c4f92c1e5dc69738d
TerryFunggg <terryyessfung@gmail.com>
2021-03-28 22:04:45 +0800

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,26 @@
-fetch("./showcase.json")
-.then(res => res.json()).then(function(showcase){
-    if(showcase === null || showcase === undefined) return;
-    appendCard(showcase);
+fetch("./showcases.json")
+.then(res => res.json()).then(function(showcases){
+    if(showcases === null || showcases === undefined) return;
+    console.log(showcases);
+    appendCard(showcases);
 }).catch(e => {
     console.log(e);
 })
 
 
-function appendCard(showcase){
+function appendCard(showcases){
+    if(showcases === undefined || showcases.length < 1) return;
+
     let html = '';
-    showcase.map(function(item){
+    showcases.map(function(item){
         html = `
 <div class="showcase">
-    <a href="./showcase/${item.html}">
-    <img src="./img/${item.img}" alt="">
+    <a href="./showcases/${item.html}">
+    <img class='object-cover h-full' src="./img/${item.img}" alt="">
     </a>
 </div>`
+document.querySelector('main').innerHTML += html;
     });
     // TODO: limit?
-    document.querySelector('main').innerHTML = html;
-}
-
-window.onload = function(){
-    document.body.classList.remove('hidden');
+    
 }
